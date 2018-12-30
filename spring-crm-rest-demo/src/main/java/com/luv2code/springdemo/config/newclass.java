707d9008266ad0c96b37decc82228d88a859569d
rajeshsoni256 <soni.raj255@gmail.com>
2018-12-30 18:41:07 +0530

    new variable added

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/config/newclass.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/config/newclass.java
@@ -3,8 +3,10 @@ package com.luv2code.springdemo.config;
 public class newclass {
 
 	public static void main(String[] args) {
-	
+		
+		Integer Ignore;
 		System.out.println("hi vivek changes here!!!");
 		System.out.println("hello rajesh change here!!!!");
+		
 	}
 }
