f58b8b7a54844aa5f77ca57694aeac4c1428e911
vickyRepo <46105324+vickyRepo@users.noreply.github.com>
2018-12-30 18:29:49 +0530

    modified Class name to meet the java coding standards

---
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/config/Newclass.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo.config;
+
+public class Newclass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("hi vivek changes here!!!");
+		System.out.println("hello rajesh change here!!!!");
+	}
+}
